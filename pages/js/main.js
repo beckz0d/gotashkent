af2a30f2144c3d609d7e9472439a89c0aba1289e
Bekzod Rakhmanov <71717959+beckz0d@users.noreply.github.com>
2023-04-13 00:58:24 +0500

    init start

---
+++ pages/js/main.js
@@ -0,0 +1,6 @@
+// const changeLogo = document.querySelector('.logo-nav')
+// changeLogo.addEventListener('mouseover', ()=> {
+//     const createNewLogo = document.createElement('div')
+//     createNewLogo.classList.add('logo-nav')
+//     changeLogo.append(createNewLogo)
+// })
